667790ba4af8feb056be74dd8568f0a487570632
Ingvar Aasen <iaasen@hotmail.com>
2017-06-15 15:26:42 +0200

    AbstractEntity implements ModelInterface. Makes it possible to save an Entity to database using AbstractTable

--- src/Oppned/AbstractEntity.php
+++ src/Oppned/AbstractEntity.php
@@ -11,6 +11,7 @@ namespace Oppned;
 use \Oppned\Entity\DateTime;
 use Exception;
 use phpDocumentor\Reflection\DocBlockFactory;
+use ReflectionClass;
 use ReflectionObject;
 use ReflectionProperty;
 
@@ -21,10 +22,10 @@ use ReflectionProperty;
  * Intended to be the simplest possible object with possibility for type casting and setters.
  * Intended to be used with REST services where an object with public attributes are expected
  * Properties should be defined as public
- * Setter functionality only available through __construct and exchangeArray
+ * Setter functionality only available through __construct, __set and exchangeArray
  *
  */
-class AbstractEntity
+class AbstractEntity implements ModelInterface
 {
 	/** @var DocBlockFactory  */
 	private $docBlockFactory;
@@ -41,59 +42,7 @@ class AbstractEntity
 
 	public function exchangeArray($data) {
 		foreach($data AS $key => $value) {
-			// Look for setter method (setField())
-			$setterName = 'set' . ucfirst($key);
-			if(method_exists($this, $setterName)) {
-				$this->$setterName($value);
-				continue;
-			}
-
-			// All properties must be predefined
-			if(!property_exists($this, $key)) {
-				if($this->throwExceptionOnMissingProperty) throw new Exception("Property '$key' not found in " . get_class($this), 400);
-				else continue;
-			}
-
-			// Populate according to @var doc-comment
-			$reflection = new ReflectionProperty($this, $key);
-			//$factory  = DocBlockFactory::createInstance();
-			$annotation = $this->docBlockFactory->create($reflection->getDocComment());
-			if(!$annotation->hasTag('var')) throw new \LogicException("Property '$key' must have a @var tag in " . get_class($this), 500);
-			/** @var \phpDocumentor\Reflection\DocBlock\Tags\Var_ $tag */
-			$tag = $annotation->getTagsByName('var')[0];
-			switch($tag->getType()) {
-				case 'bool':
-					$this->$key = (bool) $value;
-					break;
-				case 'int':
-					$this->$key = strlen($value) ? (int) $value : null;
-					break;
-				case 'float':
-					$this->$key = (float) $value;
-					break;
-				case 'string':
-					$this->$key = (string) $value;
-					break;
-				case 'string[]':
-					if(is_string($value)) {
-						if(in_array(substr($value, 0, 1), ['{', '['])) $this->$key = json_decode($value);
-					}
-					else $this->$key = $value;
-					break;
-				case '\DateTime':
-				case 'DateTime':
-					if(is_null($value)) $this->$key = null;
-					elseif(is_string($value)) $this->$key = new DateTime($value);
-					elseif($value instanceof \DateTime) $this->$key = new DateTime($value->format('c'));
-					elseif($value instanceof DateTime) $this->$key = $value;
-					else throw new \InvalidArgumentException("Property '$key' must be a string or an instance of \\DateTime in " . get_class($this));
-					break;
-				default:
-					if(is_null($this->$key) || !is_null($value)) { // Make sure default values are not overwritten with null
-						$this->$key = $value;
-					}
-					break;
-			}
+			$this->__set($key, $value);
 		}
 	}
 
@@ -106,7 +55,142 @@ class AbstractEntity
 			$propertyName = $property->name;
 			$data[$propertyName] = $this->$propertyName;
 		}
+		return $data;
+	}
 
+	public function __get($name)
+	{
+		return $this->$name;
+	}
+
+	/**
+	 * @param string $name
+	 * @param mixed $value
+	 * @throws Exception
+	 */
+	public function __set($name, $value)
+	{
+		// Look for setter method (setField())
+		$setterName = 'set' . ucfirst($name);
+		if(method_exists($this, $setterName)) {
+			$this->$setterName($value);
+			return;
+		}
+
+		// All properties must be predefined
+		if(!property_exists($this, $name)) {
+			if($this->throwExceptionOnMissingProperty) throw new Exception("Property '$name' not found in " . get_class($this), 400);
+			else return;
+		}
+
+		// Populate according to @var doc-comment
+		$reflection = new ReflectionProperty($this, $name);
+		$annotation = $this->docBlockFactory->create($reflection->getDocComment());
+		if(!$annotation->hasTag('var')) throw new \LogicException("Property '$name' must have a @var tag in " . get_class($this), 500);
+		/** @var \phpDocumentor\Reflection\DocBlock\Tags\Var_ $tag */
+		$tag = $annotation->getTagsByName('var')[0];
+		switch($tag->getType()) {
+			case 'bool':
+				$this->$name = (bool) $value;
+				break;
+			case 'int':
+				$this->$name = strlen($value) ? (int) $value : null;
+				break;
+			case 'float':
+				$this->$name = (float) $value;
+				break;
+			case 'string':
+				$this->$name = (string) $value;
+				break;
+			case 'string[]':
+				if(is_string($value)) {
+					if(in_array(substr($value, 0, 1), ['{', '['])) $this->$name = json_decode($value);
+				}
+				else $this->$name = $value;
+				break;
+			case '\DateTime':
+			case 'DateTime':
+				if(is_null($value)) $this->$name = null;
+				elseif(is_string($value)) $this->$name = new DateTime($value);
+				elseif($value instanceof \DateTime) $this->$name = new DateTime($value->format('c'));
+				elseif($value instanceof DateTime) $this->$name = $value;
+				else throw new \InvalidArgumentException("Property '$name' must be a string or an instance of \\DateTime in " . get_class($this));
+				break;
+			default:
+				if(is_null($this->$name) || !is_null($value)) { // Make sure default values are not overwritten with null
+					$this->$name = $value;
+				}
+				break;
+		}
+	}
+
+	/**
+	 * @param string $name
+	 * @return bool
+	 */
+	public function __isset($name)
+	{
+		if(!property_exists($this, $name)) return false;
+		$reflection = new ReflectionProperty($this, $name);
+		if(!$reflection->isPublic()) return false;
+		return true;
+	}
+
+	/**
+	 * @param string $name
+	 * @return void
+	 */
+	public function __unset($name)
+	{
+		if(property_exists($this, $name)) {
+			$reflection = new ReflectionProperty($this, $name);
+			if($reflection->isPublic()) unset($this->$name);
+		}
+	}
+
+	/**
+	 * @return void
+	 */
+	public function __clone()
+	{
+	}
+
+	public function __toString()
+	{
+		$data = get_class($this) . PHP_EOL;
+		$data .= @rt($this->databaseSaveArray());
+		return $data;
+	}
+
+	public function databaseSaveArray()
+	{
+		$reflection = new ReflectionClass($this);
+		$properties = $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
+		$data = [];
+		foreach($properties AS $property) {
+			if(!$property->isStatic()) {
+				$name = $property->getName();
+				$value = $this->__get($name);
+				$factory  = \phpDocumentor\Reflection\DocBlockFactory::createInstance();
+				$annotation = $factory->create($property->getDocComment());
+				/** @var \phpDocumentor\Reflection\DocBlock\Tags\Var_ $tag */
+				$tag = $annotation->getTagsByName('var')[0];
+				switch($tag->getType()) {
+					case 'bool':
+						$data[$name] = ($value) ? 1 : 0;
+						break;
+					case '\DateTime':
+					case 'DateTime';
+						/** @var DateTime $value */
+						if($value) $data[$name] = $value->format('c');
+						else $data[$name] = null;
+						break;
+					default:
+						$data[$name] = $value;
+						break;
+				}
+			}
+		}
 		return $data;
 	}
 }
