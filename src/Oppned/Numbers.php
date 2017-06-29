e89dbe6053674ca0c31c2aa774fe3b19ca4949d1
Ingvar Aasen <iaasen@hotmail.com>
2017-06-29 14:13:25 +0200

    New function secondsToHumanReadable()

---
+++ src/Oppned/Numbers.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ingvar.aasen
+ * Date: 29.06.2017
+ * Time: 12:02
+ */
+
+namespace Oppned;
+
+
+class Numbers
+{
+	static function secondsToHumanReadable($seconds) {
+		if ($seconds == 0) return '0 s';
+		else {
+			$s = $seconds % 60;
+			$m = floor(($seconds % 3600) / 60);
+			$h = floor(($seconds % 86400) / 3600);
+			$d = floor($seconds / 86400);
+			//$d = floor(($up%2592000)/86400);
+			//$M = floor($up/2592000);
+			$txt = '';
+			//if($M) $txt = $M . 'mnd';
+			if ($d) $txt .= ' ' . $d . 'd';
+			if ($h) $txt .= ' ' . $h . 't';
+			if ($m) $txt .= ' ' . $m . 'm';
+			if ($s) $txt .= ' ' . $s . 's';
+		}
+		return ltrim($txt, ' ,');
+	}
+}
