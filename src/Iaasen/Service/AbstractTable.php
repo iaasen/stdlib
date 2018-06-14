c3e980aaeba8d134d013733cf1e4187adfcf7c9b
Ingvar Aasen <iaasen@gmail.com>
2018-06-14 22:30:00 +0200

    New functions to help retrieving collections of objects

--- src/Iaasen/Service/AbstractTable.php
+++ src/Iaasen/Service/AbstractTable.php
@@ -177,4 +177,41 @@ abstract class AbstractTable
 	public function getAdapter() {
 		return $this->primaryGateway->getAdapter();
 	}
+
+	/**
+	 * Returns an array where they key is the the key from the objects and the value is an array of all objects with that key.
+	 * Useful for collecting dependencies from the database
+	 * @param object[] $objects
+	 * @param string $key
+	 * @return object[]
+	 */
+	protected function getObjectKeyMatrix(array $objects, string $key = 'id') : array {
+		return array_reduce($objects, function($carry, $item) use($key) {
+			$carry[$item->$key][] = $item;
+			return $carry;
+		}, []);
+
+//		//Does the same as the above code
+//		$objectKeyMatrix = [];
+//		foreach($objects AS $object) {
+//			$objectKeyMatrix[$object->$key][] = $object;
+//		}
+//		return $objectKeyMatrix;
+	}
+
+	/**
+	 * @param object[] $objectKeyMatrix The object matrix given by getObjectKeyMatrix()
+	 * @param string $objectFunction What function to call on parent object
+	 * @param object[] $childObjects The child objects to populate
+	 * @param string $childKey The child object attribute used to match against the object matrix
+	 */
+	protected function populateObjectKeyMatrixWithFunctionCall(array $objectKeyMatrix, string $objectFunction, array $childObjects, string $childKey) : void {
+		foreach($childObjects AS $childObject) {
+			if(isset($objectKeyMatrix[$childObject->$childKey])) {
+				foreach($objectKeyMatrix[$childObject->$childKey] AS $object) {
+					$object->$objectFunction($childObject);
+				}
+			}
+		}
+	}
 }
