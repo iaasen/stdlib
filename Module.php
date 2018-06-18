32650a930e50a6a9ee9919bfc001ca20eb6e60c4
Ingvar Aasen <ingvar.aasen@nte.no>
2018-06-18 15:32:59 +0200

    Modified Reflectiaon based factory to also deliver form elements

--- Module.php
+++ Module.php
@@ -9,49 +9,8 @@
  */
 namespace Oppned;
 
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\Db\TableGateway\TableGateway;
-use Zend\Db\ResultSet\ResultSet;
 
-class Module implements AutoloaderProviderInterface {
+class Module extends \Iaasen\Module {
 
-	public function getAutoloaderConfig() {
-		return [
-//			'Zend\Loader\ClassMapAutoloader' => array (
-//				__DIR__ . '/autoload_classmap.php'
-//			),
-			'Zend\Loader\StandardAutoloader' => [
-				'namespaces' => [
-					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__
-				]
-			]
-		];
-	}
 
-	public function getConfig() {
-		return include __DIR__ . '/config/module.config.php';
-	}
-	
-	public function getServiceConfig() {
-		return [
-			'factories' => [
-//				'Logger' => function ($sm) {
-//					$logTable = $sm->get('LogTable');
-//					$userTable = $sm->get('UserTable');
-//					return new Logger($logTable, $userTable);
-//				},
-//				'LogTable' => function ($sm) {
-//					$tableGateway = $sm->get('LogTableGateway');
-//					$table = new \General\LogTable($tableGateway);
-//					return $table;
-//				},
-//				'LogTableGateway' => function ($sm) {
-//					$dbAdapter = $sm->get('Db\Boligkalk');
-//					$resultSetPrototype = new ResultSet();
-//					$resultSetPrototype->setArrayObjectPrototype(new \Oppned\Log());
-//					return new TableGateway('Log', $dbAdapter, null, $resultSetPrototype);
-//				},
-			]
-		];
-	}
 }
