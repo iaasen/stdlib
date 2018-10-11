92da4e47970ae6d863d3e74da52bbea4611ad952
Ingvar Aasen <ingvar.aasen@nte.no>
2018-10-11 13:46:57 +0200

    Use \Iaasen\Time to format the dates with norwegian weekday in the views

---
+++ src/Iaasen/Time.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * User: ingvar.aasen
+ * Date: 11.10.2018
+ * Time: 12:50
+ */
+
+namespace Iaasen;
+
+
+
+use IntlDateFormatter;
+
+class Time
+{
+	const DATE_FORMATS = [
+		1 => 'EEEE dd.MM.YYYY HH:mm', // onsdag 17.10.2018 15.01
+		2 => 'dd.MM.YYYY HH:mm EEEE', // 17.10.2018 15.01 onsdag
+	];
+
+	public static function format(\DateTime $dateTime, int $format = 1) : string {
+		$dateFormatter = new IntlDateFormatter('nb_NO', IntlDateFormatter::FULL, IntlDateFormatter::SHORT);
+		$dateFormatter->setPattern(self::DATE_FORMATS[$format]);
+		return $dateFormatter->format($dateTime);
+	}
+}
