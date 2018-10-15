2316083a38824897a62a930320fbda48fcd52d3c
Ingvar Aasen <ingvar.aasen@nte.no>
2018-10-15 12:52:54 +0200

    Default time with colon between hours and minutes

--- src/Iaasen/Time.php
+++ src/Iaasen/Time.php
@@ -14,12 +14,12 @@ use IntlDateFormatter;
 class Time
 {
 	const DATE_FORMATS = [
-		1 => 'EEEE dd.MM.YYYY HH:mm', // onsdag 17.10.2018 15.01
-		2 => 'dd.MM.YYYY HH:mm EEEE', // 17.10.2018 15.01 onsdag
+		1 => "EEEE dd.MM.YYYY HH':'mm", // onsdag 17.10.2018 15:01
+		2 => "dd.MM.YYYY HH':'mm EEEE", // 17.10.2018 15:01 onsdag
 	];
 
 	public static function format(\DateTime $dateTime, int $format = 1) : string {
-		$dateFormatter = new IntlDateFormatter('nb_NO', IntlDateFormatter::FULL, IntlDateFormatter::SHORT);
+		$dateFormatter = new IntlDateFormatter('nb_NO', IntlDateFormatter::SHORT, IntlDateFormatter::SHORT);
 		$dateFormatter->setPattern(self::DATE_FORMATS[$format]);
 		return $dateFormatter->format($dateTime);
 	}
