dcbe3dcb22f4442e9cd3159b6747c86f77f31223
Ingvar Aasen <ingvar.aasen@nte.no>
2018-10-18 14:49:07 +0200

    New time format

--- src/Iaasen/Time.php
+++ src/Iaasen/Time.php
@@ -16,6 +16,7 @@ class Time
 	const DATE_FORMATS = [
 		1 => "EEEE dd.MM.YYYY HH':'mm", // onsdag 17.10.2018 15:01
 		2 => "dd.MM.YYYY HH':'mm EEEE", // 17.10.2018 15:01 onsdag
+		3 => "dd.MM.YYYY HH':'mm", // 17.10.2018 15:01
 	];
 
 	public static function format(\DateTime $dateTime, int $format = 1) : string {
