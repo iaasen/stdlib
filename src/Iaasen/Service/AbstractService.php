890a28d9e39a4e38f2a05da9e8c650f5a143ec4e
Ingvar Aasen <ingvar.aasen@nte.no>
2021-12-03 12:30:07 +0100

    Move ObjectKeyMatrix to separate static class. The ObjectKeyMatrix function could be used in other places also

--- src/Iaasen/Service/AbstractService.php
+++ src/Iaasen/Service/AbstractService.php
@@ -9,42 +9,30 @@
 namespace Iaasen\Service;
 
 
+use Iaasen\Service\ObjectKeyMatrix;
+
 class AbstractService
 {
 	/**
 	 * Returns an array where they key is the they keys from the objects and the value is an array of all objects with that key.
 	 * Useful for collecting dependencies from the database
-	 * @param mixed[] $objects
-	 * @param string $key
-	 * @return mixed[]
+	 * @param array $objects
+	 * @param string $key Element in the objects to index
+	 * @return array Objects grouped by $key. ($key is the array key, array of objects is the array value)
+	 * @deprecated It's a static function and better used in a static class usable from anywhere
 	 */
 	protected function getObjectKeyMatrix(array $objects, string $key = 'id') : array {
-		return array_reduce($objects, function($carry, $item) use($key) {
-			$carry[$item->$key][] = $item;
-			return $carry;
-		}, []);
-
-//		//Does the same as the above code
-//		$objectKeyMatrix = [];
-//		foreach($objects AS $object) {
-//			$objectKeyMatrix[$object->$key][] = $object;
-//		}
-//		return $objectKeyMatrix;
+		return ObjectKeyMatrix::getObjectKeyMatrix($objects, $key);
 	}
 
 	/**
-	 * @param mixed[] $objectKeyMatrix The object matrix given by getObjectKeyMatrix()
-	 * @param string $objectFunction What function to call on parent object
-	 * @param mixed[] $childObjects The child objects to populate
+	 * @param array $objectKeyMatrix The object matrix given by getObjectKeyMatrix()
+	 * @param string $objectFunction What function to call on parent object to add child object
+	 * @param array $childObjects The child objects to populate
 	 * @param string $childKey The child object key used to match against the object matrix
+	 * @deprecated It's a static function and better used in a static class usable from anywhere
 	 */
 	protected function populateObjectKeyMatrixWithFunctionCall(array $objectKeyMatrix, string $objectFunction, array $childObjects, string $childKey) : void {
-		foreach($childObjects AS $childObject) {
-			if(isset($objectKeyMatrix[$childObject->$childKey])) {
-				foreach($objectKeyMatrix[$childObject->$childKey] AS $object) {
-					$object->$objectFunction($childObject);
-				}
-			}
-		}
+		ObjectKeyMatrix::populateObjectKeyMatrixWithFunctionCall($objectKeyMatrix, $objectFunction, $childObjects, $childKey);
 	}
 }
---
+++ src/Iaasen/Service/ObjectKeyMatrix.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * User: ingvar
+ * Date: 03.12.2021
+ * Time: 10.53
+ */
+
+namespace Iaasen\Service;
+
+class ObjectKeyMatrix
+{
+	/**
+	 * Returns an array where they key is the they keys from the objects and the value is an array of all objects with that key.
+	 * Useful for collecting dependencies from the database
+	 * @param array $objects
+	 * @param string $key Element in the objects to index by
+	 * @return array Objects grouped by $key. ($key is the array key, array of objects is the array value)
+	 */
+	public static function getObjectKeyMatrix(array $objects, string $key = 'id') : array {
+		return array_reduce($objects, function($carry, $item) use($key) {
+			$carry[$item->$key][] = $item;
+			return $carry;
+		}, []);
+	}
+
+	/**
+	 * @param array $objectKeyMatrix The object matrix given by getObjectKeyMatrix()
+	 * @param string $objectFunction What function to call on parent object to add child object
+	 * @param array $childObjects The child objects to populate
+	 * @param string $childKey The child object key used to match against the object matrix
+	 */
+	public static function populateObjectKeyMatrixWithFunctionCall(array $objectKeyMatrix, string $objectFunction, array $childObjects, string $childKey) : void {
+		foreach($childObjects AS $childObject) {
+			if(isset($objectKeyMatrix[$childObject->$childKey])) {
+				foreach($objectKeyMatrix[$childObject->$childKey] AS $object) {
+					$object->$objectFunction($childObject);
+				}
+			}
+		}
+	}
+}
