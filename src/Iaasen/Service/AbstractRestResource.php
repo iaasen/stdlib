f36e83a9988eaf72d37eda99f108908a8dca0770
ingvar <ingvar.aasen@nte.no>
2022-09-09 12:06:29 +0200

    Clean out unused code in AbstractRestResource.php

--- src/Iaasen/Service/AbstractRestResource.php
+++ src/Iaasen/Service/AbstractRestResource.php
@@ -1,120 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
  * User: ingvar.aasen
  * Date: 30.06.2017
- * Time: 16:11
  */
 
 namespace Iaasen\Service;
 
 
-use Laminas\ApiTools\ApiProblem\ApiProblem;
 use Laminas\ApiTools\Rest\AbstractResourceListener;
 
 abstract class AbstractRestResource extends AbstractResourceListener
 {
-	/**
-	 * Create a resource
-	 *
-	 * @param  mixed $data
-	 * @return ApiProblem|mixed
-	 */
-	public function create($data)
-	{
-		return new ApiProblem(405, 'The POST method has not been defined');
-	}
-
-	/**
-	 * Delete a resource
-	 *
-	 * @param  mixed $id
-	 * @return ApiProblem|mixed
-	 */
-	public function delete($id)
-	{
-		return new ApiProblem(405, 'The DELETE method has not been defined for individual resources');
-	}
-
-	/**
-	 * Delete a collection, or members of a collection
-	 *
-	 * @param  mixed $data
-	 * @return ApiProblem|mixed
-	 */
-	public function deleteList($data)
-	{
-		return new ApiProblem(405, 'The DELETE method has not been defined for collections');
-	}
-
-	/**
-	 * Fetch a resource
-	 *
-	 * @param  mixed $id
-	 * @return ApiProblem|mixed
-	 */
-	public function fetch($id)
-	{
-		return new ApiProblem(405, 'The GET method has not been defined for individual resources');
-	}
-
-	/**
-	 * Fetch all or a subset of resources
-	 *
-	 * @param  array $params
-	 * @return ApiProblem|mixed
-	 */
-	public function fetchAll($params = [])
-	{
-		return new ApiProblem(405, 'The GET method has not been defined for collections');
-	}
-
-	/**
-	 * Patch (partial in-place update) a resource
-	 *
-	 * @param  mixed $id
-	 * @param  mixed $data
-	 * @return ApiProblem|mixed
-	 */
-	public function patch($id, $data)
-	{
-		return new ApiProblem(405, 'The PATCH method has not been defined for individual resources');
-	}
-
-	/**
-	 * Patch (partial in-place update) a collection or members of a collection
-	 *
-	 * @param  mixed $data
-	 * @return ApiProblem|mixed
-	 */
-	public function patchList($data)
-	{
-		return new ApiProblem(405, 'The PATCH method has not been defined for collections');
-	}
-
-	/**
-	 * Replace a collection or members of a collection
-	 *
-	 * @param  mixed $data
-	 * @return ApiProblem|mixed
-	 */
-	public function replaceList($data)
-	{
-		return new ApiProblem(405, 'The PUT method has not been defined for collections');
-	}
-
-	/**
-	 * Update a resource
-	 *
-	 * @param  mixed $id
-	 * @param  mixed $data
-	 * @return ApiProblem|mixed
-	 */
-	public function update($id, $data)
-	{
-		return new ApiProblem(405, 'The PUT method has not been defined for individual resources');
-	}
-
 
 	/**
 	 * Returns an array with start and end times in unix timestamp
@@ -158,6 +54,7 @@ abstract class AbstractRestResource extends AbstractResourceListener
 
 		return [(int) $start, (int) $end];
 	}
+
 
 	protected function requestForSingleMonth() {
 		$event = $this->getEvent();
