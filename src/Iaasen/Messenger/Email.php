58094a3607d085c698145361109d7ba67db89f38
iaasen <iaasen@gmail.com>
2025-01-16 00:36:07 +0100

    Clean up Messenger\Email class

--- src/Iaasen/Messenger/Email.php
+++ src/Iaasen/Messenger/Email.php
@@ -1,36 +1,36 @@
 <?php
 /**
- * Created by PhpStorm.
  * User: iaase
  * Date: 18.04.2018
- * Time: 21:06
  */
 
 namespace Iaasen\Messenger;
 
+use Iaasen\Exception\InvalidArgumentException;
+use Iaasen\Model\AbstractEntityV2;
 
-use Iaasen\Model\AbstractEntity;
-
-class Email extends AbstractEntity
+/**
+ * @property string $from Sender email address
+ * @property array $to Recipient email addresses
+ * @property string $subject Email subject
+ * @property string $body Plain text email body
+ * @property string $body_html HTML email body
+ */
+class Email extends AbstractEntityV2
 {
-	/** @var  string */
-	public $from;
-	/** @var  string[] */
-	public $to;
-	/** @var  string */
-	public $subject;
-	/** @var  string */
-	public $body;
-	/** @var  string */
-	public $body_html;
-//	/** @var  array */
-//	public $attachments;
+	protected string $from;
+	/** @var string[] */
+	protected array $to;
+	protected string $subject;
+	protected string $body;
+	protected string $body_html;
 
-	public function setTo($value) {
+	public function setTo(string|array $value): void {
 		if(is_string($value)) $value = [$value];
 		foreach($value as $row) {
-			if(!filter_var($row, FILTER_VALIDATE_EMAIL)) throw new \InvalidArgumentException($row . ' is not a valid email address', 400);
+			if(!filter_var($row, FILTER_VALIDATE_EMAIL)) throw new InvalidArgumentException($row . ' is not a valid email address', 400);
 		}
 		$this->to = $value;
 	}
+
 }
