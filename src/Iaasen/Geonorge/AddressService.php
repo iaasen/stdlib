c2de9964a13914a5a29df911b17259c947bfc9bc
ingvar <ingvar.aasen@nte.no>
2025-06-03 14:19:46 +0200

    Readd Geonorge\AddressService for backward compatibility. Extend the class in iaasen/geonorge

---
+++ src/Iaasen/Geonorge/AddressService.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * User: ingvar.aasen
+ * Date: 13.05.2019
+ */
+
+namespace Iaasen\Geonorge;
+
+/**
+ * @deprecated Use the new iaasen/geonorge package
+ */
+class AddressService extends \Iaasen\Geonorge\Rest\AddressService {}
